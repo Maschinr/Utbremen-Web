ca509e09718d273aa27a153d4123b26cbfcf015d
Pascal-Laptop <pascalgebauer96@gmail.com>
2018-04-19 14:24:35 +0200

    DIe Custom-Scrollbar durch eine andere ersetzt, welche von allen Browsern supported wird. Außerdem kann man jetzt mit den Pfeiltasten durch die Carousels navigieren.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  $('#fullpage').fullpage();
+$(document).ready(function () {
+    $('#fullpage').fullpage();
 });
+
+$(document).keydown(function (e) {
+    if (e.keyCode === 37) {
+        $(".carousel-control-prev").click();
+    }
+    if (e.keyCode === 39) {
+        $(".carousel-control-next").click();
+    }
+});
+
+
