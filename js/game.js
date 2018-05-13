b137f4887a7aa2b81efdbaaff08f1a86ecfdad13
Unknown <dneuke@gmail.com>
2018-05-13 13:25:03 +0200

    Snake läuft jetzt auf mehreren Canvases
    Das Spiel wird jetzt auf jeder Seite angezeigt und die Bilder könnten  nun separat geändert werden

--- js/game.js
+++ js/game.js
@@ -1,112 +1,164 @@
 var jcanvas = $("#pixelart-game");
 var canvas = jcanvas[0];
 var context = canvas.getContext("2d");
-
-jcanvas.width(jcanvas.height());
-
 var resizeId;
-$(window).resize(function() {
-    clearTimeout(resizeId);
-    resizeId = setTimeout(doneResizing, 500);
-});
+var tilewidth;
+var tileheight;
+var cursnack;
+var anchor;
+var updatetimeout;
 
 function doneResizing(){
     jcanvas.width(jcanvas.height());
+    tilewidth = canvas.width / 30;
+    tileheight = canvas.height / 30;
 }
-
-var width = canvas.width / 30;
-var height = canvas.height / 30;
-
-
 
 function body() {
     this.x = 0;
     this.y = 0;
 }
 
-function snack() {
-    this.x = width * Math.floor(Math.random() * 29);
-    this.y = height * Math.floor(Math.random() * 29);
-    console.log("new snack: " + this.x + " " + this.y);
-
+function collide(a, b) {
+    if(a.x === b.x && a.y === b.y) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+function snack() {
+    var created = false;
 
+    while(created === false) {
+        this.x = tilewidth * Math.floor(Math.random() * 29);
+        this.y = tileheight * Math.floor(Math.random() * 29);
+        for(var i = 0; i < anchor.bodyparts.length; i++) {
+            if (collide(this, anchor.bodyparts[i])) {
+                created = false;
+            } else {
+                created = true;
+                break;
+            }
+        }
+    }
+    
+    console.log("new snack: " + this.x + " " + this.y);
+}
 
 function snake() {
     this.bodyparts = [this];
     this.x = canvas.width / 2;
     this.y = canvas.height / 2;
     this.direction = 0;
+    this.newdirection = 0;
 
     this.move = function() {
+        if(this.newdirection != 0) {
+            this.direction = this.newdirection;
+        }
+        this.newdirection = 0;
+
         for(var i = this.bodyparts.length - 1; i !== 0; i--) {
 
             this.bodyparts[i].y = this.bodyparts[i - 1].y;
             this.bodyparts[i].x = this.bodyparts[i - 1].x;
         }
-        if(this.direction === 0) { // Up
-            this.y -= height;
-        } else if(this.direction === 1) { // Right
-            this.x -= width;
-        } else if(this.direction === 2) { // Down
-            this.y += height;
-        } else if(this.direction === 3) { // Left
-            this.x += width;
-        }
+        if(this.direction === "up") {
+            this.y -= tileheight;
+        } else if(this.direction === "left") {
+            this.x -= tilewidth;
+        } else if(this.direction === "down") {
+            this.y += tileheight;
+        } else if(this.direction === "right") {
+            this.x += tilewidth;
+        } 
     }
 
     this.draw = function() {
         for(var i = 0; i < this.bodyparts.length; i++) {
             context.fillStyle = "#FFFFFF";
-            context.fillRect(this.bodyparts[i].x, this.bodyparts[i].y, width, height);
-
+            context.fillRect(this.bodyparts[i].x, this.bodyparts[i].y, tilewidth, tileheight);
         }
     }
 }
 
-var anchor = new snake();
-
-window.addEventListener("keydown", this.move, false);
-
-update();
-
 function move(e) {
-
     if(e.keyCode === 87) { // W
-        anchor.direction = 0;
+        if(anchor.direction != "down") {
+            anchor.newdirection = "up";
+        }
     } else if(e.keyCode === 65) { // A
-        anchor.direction = 1;
+        if(anchor.direction != "right") {
+            anchor.newdirection = "left";
+        }
     } else if(e.keyCode === 83) { // S
-        anchor.direction = 2;
+        if(anchor.direction != "up") {
+            anchor.newdirection = "down";
+        }
     } else if(e.keyCode === 68) { // D
-        anchor.direction = 3;
+        if(anchor.direction != "left") {
+            anchor.newdirection = "right";
+        }
     }
-
 }
-
-var cursnack;
 
 function update() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-            //Draw Player
+    //Snack logic
     if(cursnack) {
         if(anchor.x === cursnack.x && anchor.y === cursnack.y) {
             cursnack = null;
             anchor.bodyparts.push(new body());
         }
     } else {
-            cursnack = new snack();
+        cursnack = new snack();
     }
+
+    //Movement per tick
     anchor.move();
 
-    if(cursnack) {
-        context.fillStyle = "#FF0000";
-        context.fillRect(cursnack.x, cursnack.y, width, height);
+    //Check if lost
+    for(var i = 1; i < anchor.bodyparts.length; i++) {
+        if (collide(anchor, anchor.bodyparts[i])) {
+            gameover();
+        }
     }
 
+    if(anchor.x > tilewidth * 29 || anchor.x < 0 || anchor.y > tileheight * 29 || anchor.y < 0) {
+        gameover();
+    }
+
+    //Drawing
+    if(cursnack) {
+        context.fillStyle = "#FF0000";
+        context.fillRect(cursnack.x, cursnack.y, tilewidth, tileheight);
+    }
     anchor.draw();
 
-    window.setTimeout(update, 100);
+    updatetimeout = setTimeout(update, 100);
 }
+
+function gameover() {
+    alert("You Lost!");
+    anchor = new snake();
+    cursnack = new snack();
+}
+
+function gamestart(canvasname) {
+    clearTimeout(updatetimeout);
+    jcanvas = $(canvasname);
+    canvas = jcanvas[0];
+    context = canvas.getContext("2d");
+    window.addEventListener("keydown", move, false); // Start input hook
+    doneResizing(); //Set width and height
+    anchor = new snake();
+    cursnack = new snack();
+    update(); // Start update loop
+}
+
+$(window).resize(function() {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 500);
+});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage();
+  $("#fullpage").fullpage({
+    onLeave: function(index, nextIndex, direction) {
+      console.log("onLeave--" + "index: " + index + " nextIndex: " + nextIndex + " direction: " +  direction);
+      if(nextIndex === 2) {
+        gamestart('#pixelart-game');
+      } else if(nextIndex === 3) {
+        gamestart('#hand-drawn-game');
+      }
+    }
+  });
 });
